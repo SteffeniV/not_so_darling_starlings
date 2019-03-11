aba5504d35da6e8a6ce3049d91c61e4b3549bc0a
Steffeni Bunsick <steffenibunsick@gmail.com>
2019-03-10 23:00:18 -0500

    chart renders by selected state

--- client/components/byState.js
+++ client/components/byState.js
@@ -78,6 +78,59 @@ export class ByState extends Component {
     WY: "Wyoming"
   }
 
+  oppStateDict = {
+    Alabama: "AL",
+    Alaska: "AK",
+    Arizona: "AZ",
+    Arkansas: "AR",
+    California: "CA",
+    Colorado: "CO",
+    Connecticut: "CT",
+    Delaware: "DE",
+    Florida: "FL",
+    Georgia: "GA",
+    Hawaii: "HI",
+    Idaho: "ID",
+    Illinois: "IL",
+    Indiana: "IN",
+    Iowa: "IA",
+    Kansas: "KS",
+    Kentucky: "KY",
+    Louisiana: "LA",
+    Maine: "ME",
+    Maryland: "MD",
+    Massachusetts: "MA",
+    Michigan: "MI",
+    Minnesota: "MN",
+    Mississippi: "MS",
+    Missouri: "MO",
+    Montana: "MT",
+    Nebraska: "NE",
+    Nevada: "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    Ohio: "OH",
+    Oklahoma: "OK",
+    Oregon: "OR",
+    Pennsylvania: "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    Tennessee: "TN",
+    Texas: "TX",
+    Utah: "UT",
+    Vermont: "VT",
+    Virginia: "VA",
+    Washington: "WA",
+    "West Virginia": "WV",
+    Wisconsin: "WI",
+    Wyoming: "WY"
+  }
+
   mapHandler = event => {
     this.setState({
       selectedStates: this.state.selectedStates.concat(
@@ -109,10 +162,12 @@ export class ByState extends Component {
       yearOptions.push({key: i, text: i, value: i})
     }
     let {year} = this.state
+
     const dataForChart = this.props.starlingsByState.length
       ? this.props.starlingsByState.map(starling => {
+          console.log(starling)
           return {
-            x: this.stateDict[Object.keys(starling)],
+            x: this.oppStateDict[Object.keys(starling)],
             y: starling[Object.keys(starling)]
           }
         })
